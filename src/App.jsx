3eb2409cb72d411a84e4d543f409b494495a7fc7
eshwagadkar <eshwagadkar007@gmail.com>
2025-10-11 14:44:02 +0530

    Managing state to switch between Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import NewProject from "./components/NewProject";
-import NoProjectSelected from "./components/NoProjectSelected";
-import ProjectsSidebar from "./components/ProjectsSideBar";
+import { useState } from 'react';
+import NewProject from './components/NewProject';
+import NoProjectSelected from './components/NoProjectSelected';
+import ProjectsSidebar from './components/ProjectsSideBar';
 
 function App() {
+
+  const [ projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []    
+  })
+
+  function handleAddProject() {
+    
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+    })
+
+  }
+
+  let content
+
+  if(projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onAddProject={handleAddProject} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      {/* <NewProject /> */}
-      <NoProjectSelected />
+      <ProjectsSidebar onAddProject={handleAddProject} />
+      {content}
     </main>
   );
 }
--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,7 +1,7 @@
 import noProjectImage from '../assets/no-projects.png'
 import Button from './Button'
 
-export default function NoProjectSelected() {
+export default function NoProjectSelected({ onAddProject }) {
     return <div className="mt-4 text-center w-2/3">
         
         <img 
@@ -14,7 +14,7 @@ export default function NoProjectSelected() {
          </h2>
          <p className='text-stone-400 mb-4'>Select a project or get started with a new one</p>
          <p className='mt-8'>
-            <Button>Create a new Project</Button>
+            <Button onClick={onAddProject} >Create a new Project</Button>
         </p>
     </div>
 }
