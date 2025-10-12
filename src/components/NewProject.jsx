6b0fc1c10448913a73c6e5f31f2ccd9d29b258c1
eshwagadkar <eshwagadkar007@gmail.com>
2025-10-12 11:40:27 +0530

    Reusable Modal Component

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,5 +1,6 @@
 import { useRef } from 'react'
 import Input from './Input'
+import Modal from './Modal'
 
 export default function NewProject({onAdd}) {
  
@@ -7,12 +8,20 @@ export default function NewProject({onAdd}) {
  const description = useRef()
  const dueDate = useRef()
 
+ const modal = useRef()
+
  function handleSave(){
     const enteredTitle = title.current.value
     const enteredDescription = description.current.value
     const enteredDueDate = dueDate.current.value
 
     // Validation... 
+    if(enteredTitle.trim() === '' || enteredDescription.trim() || enteredDueDate.trim()){
+        modal.current.open()
+        return
+    }
+
+
     onAdd({
         title: enteredTitle,
         description: enteredDescription,
@@ -20,21 +29,28 @@ export default function NewProject({onAdd}) {
     })
  }
 
- return <div className="w-[35rem] mt-16">
-    <menu className="flex items-center justify-end gap-4 my-4">
-        <li><button className="text-stone-800 hover:text-strong-950">Cancel</button></li>
-        <li>
-        <button 
-            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-            onClick={handleSave}>
-            Save
-        </button>
-        </li>
-    </menu>
-    <div>
-        <Input type='text' ref={title} label='Title' />
-        <Input ref={description} label='Description' textarea />
-        <Input type='date' ref={dueDate} label='Due Date' />
+ return <>
+    <Modal ref={modal} buttonCaption='Close'>
+        <h2>Invalid Input</h2>
+        <p>Oops... looks l ike you forgot to enter a value.</p>
+        <p>Please make sure you provide a valid value for every input field.</p>
+    </Modal>
+    <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+            <li><button className="text-stone-800 hover:text-strong-950">Cancel</button></li>
+            <li>
+            <button 
+                className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+                onClick={handleSave}>
+                Save
+            </button>
+            </li>
+        </menu>
+        <div>
+            <Input type='text' ref={title} label='Title' />
+            <Input ref={description} label='Description' textarea />
+            <Input type='date' ref={dueDate} label='Due Date' />
+        </div>
     </div>
-  </div>
+ </>
 }
