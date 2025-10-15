19cf3ee4d8a21669c029f2902ad00f296508cc8e
eshwagadkar <eshwagadkar007@gmail.com>
2025-10-15 12:50:44 +0530

    filter the tasks based based on the projects selected

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -4,18 +4,20 @@ import { TaskContext } from '../store/tasks-context'
 
 export default function Tasks() {
 
-    const { tasks, handleDeleteTask } = useContext(TaskContext)
+    const { tasks, handleDeleteTask, selectedProjectId } = useContext(TaskContext)
+
+    const tasksCopy = tasks.filter(task => task.projectId === selectedProjectId)
 
     return <section>
         <h2 className='text-2xl font-bold text-stone-700 mb-4'>Tasks</h2>
         <NewTask />
-        { tasks.length === 0 && <p className='text-stone-800 my-4'>
+        { tasksCopy.length === 0 && <p className='text-stone-800 my-4'>
             This project does not have any tasks yet
         </p>
         }
-        { tasks.length > 0 && 
+        { tasksCopy.length > 0 && 
         <ul className='p-4 mt-8 rounded-md bg-stone-100'>
-            {tasks.map(task => <li key={task.id} className='flex justify-between my-4'>
+            {tasksCopy.map(task => <li key={task.id} className='flex justify-between my-4'>
                                 <span>{task.text}</span>
                                 <button onClick={() => handleDeleteTask(task.id) }
                                  className='text-stone-700 hover:text-red-500'>
